b9f015e4320bcca85a0cc515c1fdf0308f011bf9
dinndei <dinn0533@gmail.com>
2024-02-07 19:35:13 +0200

    last commit.2

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 export const connectToDB = () => {
-    const mongoURI = process.env.DB_CONNECTION || "mongodb://localhost:27017";
+    const mongoURI = process.env.DB_CONNECTION || "mongodb+srv://<Dinn0533>:<d325749398++>@firstdb.9dizht5.mongodb.net/";
     mongoose.connect(mongoURI)
         .then(suc => {
             console.log("connected ",suc.connection.host)
