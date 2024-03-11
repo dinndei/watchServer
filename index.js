c6a69c9bc1c75b74c093adf94a871c866f603fb0
dinndei <dinn0533@gmail.com>
2024-03-11 18:55:28 +0200

    change static aproach

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use("/watch", watchrouter)
 app.use("/user", userrouter)
 app.use("/order", orderrouter)
 app.use("/opinion", oprouter)
-app.use(express.static('/images'));
+app.use('/images', express.static('images'));
 let port = process.env.PORT || 5000;
 app.listen(port, console.log(`app is listening on port ${port}`));
 app.use(errorHandling);
